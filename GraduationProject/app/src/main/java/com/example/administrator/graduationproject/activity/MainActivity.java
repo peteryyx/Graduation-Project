7048b55274019a60974a9c8bf5db80a023479ed8
于亿鑫 <307365143@qq.com>
2018-03-31 18:05:39 +0800

    增加底部导航（未完成，会崩溃）

--- GraduationProject/app/src/main/java/com/example/administrator/graduationproject/activity/MainActivity.java
+++ GraduationProject/app/src/main/java/com/example/administrator/graduationproject/activity/MainActivity.java
@@ -1,20 +1,99 @@
 package com.example.administrator.graduationproject.activity;
 
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
 
-import com.bumptech.glide.Glide;
 import com.example.administrator.graduationproject.R;
+import com.example.administrator.graduationproject.adapter.MainFragmentAdapter;
 import com.example.administrator.graduationproject.base.BaseActivity;
+import com.example.administrator.graduationproject.fragment.HomeFragment;
+import com.example.administrator.graduationproject.fragment.PlayFragment;
+import com.example.administrator.graduationproject.fragment.VedioFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.vp_fragment)
+    ViewPager vpPager;
+    @BindView(R.id.tl_foot)
+    TabLayout tlFoot;
+
+    private TabLayout.Tab one;
+    private TabLayout.Tab two;
+    private TabLayout.Tab three;
+
+    private String[] tabs = {"聊天","影音","娱乐"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ImageView imageView = findViewById(R.id.image);
-        Glide.with(this).load("http://img0.imgtn.bdimg.com/it/u=1983968240,1065183412&fm=27&gp=0.jpg").into(imageView);
+        ButterKnife.bind(this);
+//        Glide.with(this).load("http://img0.imgtn.bdimg.com/it/u=1983968240,1065183412&fm=27&gp=0.jpg").into(imageView);
 
+        initTabLayout();
+
+    }
+
+    private void initTabLayout() {
+//        tabs = getApplicationContext().getResources().getStringArray(R.array.tabs);
+        tlFoot.addTab(tlFoot.newTab().setText(tabs[0]).setIcon(R.drawable.select_chat));
+        tlFoot.addTab(tlFoot.newTab().setText(tabs[1]).setIcon(R.drawable.select_vedio));
+        tlFoot.addTab(tlFoot.newTab().setText(tabs[2]).setIcon(R.drawable.unselect_paly));
+
+//        one = tlFoot.getTabAt(0);
+//        two = tlFoot.getTabAt(1);
+//        three = tlFoot.getTabAt(2);
+
+        List<Fragment> fragments = new ArrayList<>();
+        fragments.add(HomeFragment.newInstance());
+        fragments.add(VedioFragment.newInstance());
+        fragments.add(PlayFragment.newInstance());
+
+        MainFragmentAdapter mfa = new MainFragmentAdapter(getSupportFragmentManager(),fragments,tabs);
+        vpPager.setAdapter(mfa);
+
+        tlFoot.setupWithViewPager(vpPager);
+
+        tlFoot.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // TODO 可抽出来独立判断
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        vpPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                // 滑动中执行
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                // 选中position后执行
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
---
+++ GraduationProject/app/src/main/java/com/example/administrator/graduationproject/fragment/PlayFragment.java
@@ -0,0 +1,37 @@
+package com.example.administrator.graduationproject.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.administrator.graduationproject.R;
+
+/**
+ * Created by Administrator on 2018/3/31 0031.
+ */
+
+public class PlayFragment extends Fragment {
+    public static PlayFragment newInstance(){
+        return new PlayFragment();
+    }
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.play_fragment,null,false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+}
